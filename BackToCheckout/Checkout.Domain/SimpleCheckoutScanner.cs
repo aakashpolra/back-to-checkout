4214db26e3497c84cfd9e8fe8d059fb4c606d90d
Aakash <1733759+aakashpolra@users.noreply.github.com>
2024-02-11 23:13:04 +1300

    Adding extensible interface for checkout scanners and a stub implementation for the given problem details (using char data type).

---
+++ BackToCheckout/Checkout.Domain/SimpleCheckoutScanner.cs
@@ -0,0 +1,22 @@
+﻿namespace Checkout.Domain;
+
+/// <summary>
+/// Implements <see cref="ICheckoutScanner{TItemId}"/> using char type of item identification.
+/// This is a simple implementation for the given problem domain that uses simple pricing rules
+/// with optional quantity based special pricing.
+/// 
+/// Please see:
+/// http://codekata.com/kata/kata09-back-to-the-checkout/
+/// </summary>
+public class SimpleCheckoutScanner : ICheckoutScanner<char>
+{
+    public void Scan(char itemIdentifier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal GetTotal()
+    {
+        throw new NotImplementedException();
+    }
+}
