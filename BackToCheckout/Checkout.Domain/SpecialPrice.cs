f6ed10a209e9df46a6800fe8c3d1ed30d944af5b
Aakash <1733759+aakashpolra@users.noreply.github.com>
2024-02-11 23:01:39 +1300

    Adding PricingRule and SpecialPrice domain objects.

---
+++ BackToCheckout/Checkout.Domain/SpecialPrice.cs
@@ -0,0 +1,9 @@
+﻿namespace Checkout.Domain;
+
+/// <summary>
+/// Quantity based special price.
+/// E.g. 3 for $130
+/// </summary>
+/// <param name="Quantity">Quantity or the multiples of which the special price is for</param>
+/// <param name="Price">The special price for the given Quantity</param>
+public record SpecialPrice(int Quantity, decimal Price);
